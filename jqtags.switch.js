46bc1304c1aa30d399de4c9dbfdab5383c6a7c19
promil bhardwaj <improm@users.noreply.github.com>
2016-09-12 12:05:19 +0530

    Update jqtags.switch.js

--- jqtags.switch.js
+++ jqtags.switch.js
@@ -1,39 +1,43 @@
-_tag_('jqtags.switch',function(test,_test_, _attr_){
-	
-	return {
-	    tagName: "jq-switch",
-	    events: {
-	        "change .switch-input":"toggleValue"
-	    },
-	    accessors: {
-	        value: {
-	            type: "boolean"
-	        },
-	        on: {
-	            type: "string",
-	            default : "On"
-	        },
-	        off: {
-	            type: "string",
-	        	default : "Off"
-	        },
-	        label: {
-	            type: "string",
-	        	default : ""
-	        }
-	    },
-	    attachedCallback: function () {
-	        this.$.innerHTML = 
-	        '<label><span>'+this.$.label+'</span>' + 
-	        '<label class="switch"> \
-	        	<input type="checkbox" class="switch-input" '+ (this.$.value? 'checked' : '') +'> \
-	        	<span class="switch-label" data-on="'+this.$.on+'" data-off="'+this.$.off+'"></span> \
+_tag_('jqtags.switch', function (test, _test_, _attr_) {
+
+    return {
+        tagName: "jq-switch",
+        events: {
+            "change .switch-input": "toggleValue"
+        },
+        accessors: {
+            value: {
+                type: "boolean",
+                onChange: "valueSet"
+            },
+            on: {
+                type: "string",
+                default: "On"
+            },
+            off: {
+                type: "string",
+                default: "Off"
+            },
+            label: {
+                type: "string",
+                default: ""
+            }
+        },
+        attachedCallback: function () {
+            this.$.innerHTML =
+                '<label><span>' + this.$.label + '</span>' +
+                '<label class="switch"> \
+                    <input type="checkbox" class="switch-input" ' + (this.$.value ? 'checked' : '') + '> \
+	        	<span class="switch-label" data-on="' + this.$.on + '" data-off="' + this.$.off + '"></span> \
 	        	<span class="switch-handle"></span> \
 	    	</label></label>';
-	    },
-	    toggleValue : function(){
-	    	this.$.value = !this.$.value;
-	    }
-	};
-	
+        },
+        toggleValue: function () {
+            this.$.value = !this.$.value;
+        },
+        valueSet: function () {
+            this.$.querySelector(".switch-input").checked = !!this.$.value;
+        }
+    };
+
 });
